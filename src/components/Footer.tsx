601cb96f65add3ead19be869cd827830c1acd2ed
patrikrobredo <patrikrobredo9642@gmail.com>
2025-05-02 08:26:07 -0400

    update

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Footer.scss'
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <a href="https://github.com/patrikrobredo" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/patrikrobredo/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+      </div>
+      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Patrik Robredo</a> with 💜</p>
+    </footer>
+  );
+}
+
+export default Footer;
